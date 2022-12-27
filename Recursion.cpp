fba743796ae435d15112ae0679f48c406db9c06c
joel <joelkab123@gmail.com>
2022-12-27 13:03:40 -0800

    what is recursion

---
+++ Recursion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+/*
+ A Recursive function is a functions that calls itself
+
+ Type fun(param) {
+    if(<basecondition>){
+        ----
+        fun(parm);
+        ----
+    }
+ }
+*/
+
+int main()
+{
+    cout << "hello world";
+}
