0a7ad9589eb6f11d1d2ad69fbd1e35c21eabc41b
joel <joelkab123@gmail.com>
2022-12-28 13:38:43 -0800

    Indirect Recursion

--- TreeRecursion.cpp
+++ TreeRecursion.cpp
@@ -13,7 +13,7 @@ void fun(n){
     }
 }
 */
-/*-----------------------------------s--------- Tree Recursion-----------
+/*-------------------------------------------- Tree Recursion-----------
 -If a recursive function is calling itself more than one time
 
 void fun(n){
@@ -31,9 +31,23 @@ program makes 15 total function calls
 -Space - O(n) - n+1
 
 */
+/*-------------------------------------------- Indirect Recursion-----------
+- Two or more functions calling one another in a circlur fashion. for example function A calling function B while B is callng function
+C, C calling A.
 
-
-
+void funA(int n){
+    if(n>0){
+        cout<<n;
+        funB(n-1);
+    }
+}
+void funB(int n){
+    if(n>0){
+        cout<<n;
+        funA(n/2);
+    }
+}
+*/
 
 void main()
 {
