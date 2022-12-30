019f16e0d77ea20aa72861d8b80675f3395b66d6
joel <joelkab123@gmail.com>
2022-12-29 21:14:30 -0800

    Deleting elements from Array

---
+++ Array_ADT.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+/*----------------------------------Deleting elements from Array--------
+Delete(index);
+
+ A = {8,3,7,15,65, ,3,2,2332}
+- when deleting elements from an array you should replace that index with another element
+if a space is vacant, you should shift all the elements.
+-a simple for loop should do
+
+A= A[index];
+for(i = index; i<length - 1; i++){
+    A[i]= A[i+1];
+}
+length--;
+
+*/
+
+int main()
+{
+
+    cout <<"Learning more about arrays";
+}
