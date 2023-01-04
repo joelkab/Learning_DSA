a5db68add2841d463c458035a91ea4dfbe0de8f6
joel <joelkab123@gmail.com>
2023-01-03 20:39:41 -0800

    Storing Matrix

---
+++ Matrix.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+ /*
+ when storing data from a array to a matrix you need two functions 
+ first a function to get data, with 4 parameters, array, row number, row collum, element.
+
+ void Set(int A[], int i, int j, int x){
+    if(i == j){
+        A[i-1]= x;
+    }
+ }
+
+ int Get(int A[], int i, int j){
+    if( i == j){
+        return  A[i-1];
+    }
+    return 0;
+ }
+ */
+
+int main() {
+
+}
