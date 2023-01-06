d01e02eaa366cf4d83454d9dc06e40af586cdfee
joel <joelkab123@gmail.com>
2023-01-05 18:00:31 -0800

    Polynomial Representation

--- SparseMatrix&Polynomial.cpp
+++ SparseMatrix&Polynomial.cpp
@@ -22,8 +22,33 @@ A[3,8,10,4,2,6,9,5]
 IA[0, .......]
 the first element is 0
 takes the cumulative of number of elements 
+*/
 
+/*---------------------------------- Polynomial Representation ----------------
+-if we have a polynomial we can represent it as dynamic array 
 
+3x^5+2x^4+5x^2+2x+7
+
+coeff   3,2,5,2,7
+Exp     5,4,2,1,0
+struct term{
+    int coeff;
+    int Exp;
+}
+
+struct Poly{
+    int n;
+    struct term *t;
+}
+
+struct Poly p;
+cout<<"no of none zeroe term"'
+cin>> &p.n;
+p.t = new Term[p.n];
+cout<<"Enter poly Terms";
+for(i=0;i<p;i++){
+    cout<<i+1;
+}
 
 
 
