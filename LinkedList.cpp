af12b9587ffc2e97262a67d53c7be4ec2b347147
joel <joelkab123@gmail.com>
2023-01-05 21:42:24 -0800

    What is a linked List

---
+++ LinkedList.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+/*----------------------------------Linked List--------
+- A collection of node that contains data pointed to the next node
+Created inside head instead of Stack
+
+-Why Linked List instead of arrays 
+Arrays are fixed size,
+
+-Differences between array and linkedList
+A bench, like an array, has a fixed amount of individuals (elements) who can sit on it. 
+A linked list is analogous to a chair in that only one person can sit on it, but you may bring as many chairs as you like and connect them together.
+Every element will bring its own memory and point it to the next element.
+
+First is a pointer which points on the first node 
+*/
+
+
+
+int main()
+{
+    int arr[7] = {8, 9, 3, 2, 5, 6, 7};
+    int key = 5;
+}
